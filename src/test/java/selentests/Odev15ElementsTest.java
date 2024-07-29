55a976e88bf62113f1d973d318f567dcab124c16
muratbill <murat.bilal@nokia.com>
2024-07-29 19:03:11 +0300

    Odev 10 Ders14 paketi eklendi.

--- src/test/java/selentests/Odev15ElementsTest.java
+++ src/test/java/selentests/Odev15ElementsTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
